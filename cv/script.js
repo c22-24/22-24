469e1599572aa5fe9834d24caf74e5eabd4eb307
what is love <144520341+c22-24@users.noreply.github.com>
2023-12-12 06:45:14 +0200

    Delete cv directory

--- cv/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-
-document.addEventListener("DOMContentLoaded", function () {
-    // Використовуйте GSAP для анімації тексту
-    gsap.from(".title", { opacity: 0, duration: 1, y: -50, ease: "power2.out" });
-    gsap.from(".description", { opacity: 0, duration: 1, y: -50, ease: "power2.out", delay: 0.5 });
-    // Додайте інші анімації за необхідністю
-  });
-  
